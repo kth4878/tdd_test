fa3318f85372d063565b6f1e6a713c0ddb9657be
user <kth4878@naver.com>
2021-10-19 21:38:39 +0900

    refactor : 랜덤 도메인 이름변경

---
+++ src/main/java/list/domain/lottery/LotteryRandomNumber.java
@@ -0,0 +1,16 @@
+package list.domain.lottery;
+
+import java.util.Random;
+
+public class LotteryRandomNumber {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
+	private static final Random randomNumber = new Random();
+
+	public LotteryRandomNumber() {
+	}
+
+	public static int randomResult() {
+		return randomNumber.nextInt(MAX_NUMBER) + MIN_NUMBER;
+	}
+}
