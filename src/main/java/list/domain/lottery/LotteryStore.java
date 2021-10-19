12468c7acebf539eac83374f994b69ea762247c0
user <kth4878@naver.com>
2021-10-19 21:37:44 +0900

    feat : 금액별 로또 개수 관리 도메인 추가

---
+++ src/main/java/list/domain/lottery/LotteryStore.java
@@ -0,0 +1,39 @@
+package list.domain.lottery;
+
+import list.calculator.Calculation;
+
+public class LotteryStore {
+	private static final int LOTTERY_PRICE = 1000;
+	private static final String LOTTERY_PRICE_ERROR_MESSAGE = "error : 최소 금액은 1000원 입니다.";
+	private final int price;
+	private final int lotteryCount;
+
+	public LotteryStore(int price) {
+		checkLotteryPrice(price);
+		this.price = price;
+		this.lotteryCount = lotteryCalculation(discard(price));
+	}
+
+	private void checkLotteryPrice(int price) {
+		if (price < LOTTERY_PRICE) {
+			throw new IllegalArgumentException(LOTTERY_PRICE_ERROR_MESSAGE);
+		}
+	}
+
+	private int discard(int price) {
+		return Math.round(Calculation.calculate("/", price, LOTTERY_PRICE)) * LOTTERY_PRICE;
+	}
+
+	private int lotteryCalculation(int price) {
+		return Calculation.calculate("/", price, LOTTERY_PRICE);
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public int buyLottery() {
+		return this.lotteryCount;
+	}
+
+}
