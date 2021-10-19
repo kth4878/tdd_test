5f5817db57d209d81618da89b05786397a7304ec
user <kth4878@naver.com>
2021-10-19 21:38:10 +0900

    refactor : 로또번호 관리 도메인 이름변경

---
+++ src/main/java/list/domain/lottery/LotteryNumber.java
@@ -0,0 +1,23 @@
+package list.domain.lottery;
+
+public class LotteryNumber {
+	private static final String NUMBER_ERROR_MESSAGE = "error : 입력가능한 숫자가 아닙니다.";
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
+	private final int lotteryNumber;
+
+	public LotteryNumber(int lotteryNumber) {
+		checkLotteryNumber(lotteryNumber);
+		this.lotteryNumber = lotteryNumber;
+	}
+
+	private void checkLotteryNumber(int lotteryNumber) {
+		if (lotteryNumber < MIN_NUMBER || lotteryNumber > MAX_NUMBER) {
+			throw new IllegalArgumentException(NUMBER_ERROR_MESSAGE);
+		}
+	}
+
+	public int getLotteryNumber() {
+		return lotteryNumber;
+	}
+}
