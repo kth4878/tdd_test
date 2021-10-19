51e4debd1fb2d533808dabb6332a7d0ff89b31c2
user <kth4878@naver.com>
2021-10-19 21:41:06 +0900

    test : 금액별 로또구매 테스트 추가

---
+++ src/test/java/list/domain/lottery/LotteryStoreTest.java
@@ -0,0 +1,17 @@
+package list.domain.lottery;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LotteryStoreTest {
+
+	@Test
+	@DisplayName("금액에 맞는 로또개수 생성")
+	public void createLotteryTicket(){
+		LotteryStore lotteryStore = new LotteryStore(3500);
+		assertThat(lotteryStore.buyLottery()).isEqualTo(3);
+	}
+
+}
