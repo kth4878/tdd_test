2819e8a59951c1705236f7f3d0185ce451942089
user <kth4878@naver.com>
2021-10-19 21:36:18 +0900

    refactor : 도메인 등 기능변경으로 인한 View 수정

---
+++ src/main/java/list/view/lottery/InputView.java
@@ -0,0 +1,83 @@
+package list.view.lottery;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import list.domain.lottery.LotteryTicketCount;
+
+public class InputView {
+
+	private final static Scanner scanner = new Scanner(System.in);
+	private static final String LOTTERY_PRICE_MESSAGE = "로또 1장의 가격은 1000원 입니다.";
+	private static final String LOTTERY_PURCHASE_MESSAGE = "구매금액을 입력해 주세요.";
+	private static final String MANUAL_LOTTERY_MESSAGE = "수동 로또 개수를 입력하세요.";
+	private static final String MANUAL_LOTTERY_NUMBERS_MESSAGE = "로또 번호를 입력하세요 (,으로 구분)";
+	private static final String INPUT_TYPE_ERROR_MESSAGE = "error : 아라비아 숫자만 입력 가능합니다.";
+	private static final String LOTTERY_LENGTH_ERROR_MESSAGE = "error : 로또 번호는 6개 입력해야 합니다.";
+	private static final String LAST_WEEK_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력하세요. (,으로 구분)";
+	private static final String COMMA = ",";
+	private static final int ZERO = 0;
+
+	public static void lotteryPrice() {
+		System.out.println(LOTTERY_PRICE_MESSAGE);
+	}
+
+	public static int inputLotteryPurchasePrice() {
+		System.out.println(LOTTERY_PURCHASE_MESSAGE);
+
+		return checkInputTypeNumber();
+	}
+
+	public static int inputManualLotteryCount() {
+		System.out.println(MANUAL_LOTTERY_MESSAGE);
+
+		return checkInputTypeNumber();
+	}
+
+	public static List<String[]> inputManualLotteryNumbers(LotteryTicketCount lotteryTicketCount) {
+		if (lotteryTicketCount.getManualLotteryCount() != ZERO) {
+			System.out.println(MANUAL_LOTTERY_NUMBERS_MESSAGE);
+		}
+
+		List<String[]> lotteryNumbers = inputLotteryNumber(lotteryTicketCount.getManualLotteryCount());
+
+		checkLotteryNumbers(lotteryNumbers);
+
+		return lotteryNumbers;
+	}
+
+	public static List<String[]> inputLastWeekLotteryNumbers() {
+		System.out.println(LAST_WEEK_WINNING_NUMBER_MESSAGE);
+		return inputLotteryNumber(1);
+	}
+
+	private static int checkInputTypeNumber() {
+		if (!scanner.hasNextInt()) {
+			throw new IllegalArgumentException(INPUT_TYPE_ERROR_MESSAGE);
+		}
+
+		int number = scanner.nextInt();
+		scanner.nextLine();
+
+		return number;
+	}
+
+	private static void checkLotteryNumbers(List<String[]> lotteryNumbers) {
+		long count = lotteryNumbers.stream()
+			.filter(i -> i.length == 6)
+			.count();
+
+		if (count != lotteryNumbers.size()) {
+			throw new IllegalArgumentException(LOTTERY_LENGTH_ERROR_MESSAGE);
+		}
+	}
+
+	private static List<String[]> inputLotteryNumber(int number) {
+		return Stream.generate(() -> scanner.nextLine().split(COMMA))
+			.limit(number)
+			.collect(Collectors.toList());
+	}
+
+}
---
+++ src/main/java/list/view/lottery/OutputView.java
@@ -0,0 +1,41 @@
+package list.view.lottery;
+
+import list.calculator.LotteryPrize;
+import list.domain.lottery.LotteryRanking;
+import list.domain.lottery.LotteryTicketCount;
+import list.domain.lottery.LotteryTickets;
+
+public class OutputView {
+
+	private static final String BUY_LOTTERY_TICKET_MESSAGE = "수동으로 %d장, 자동으로 %d장 을 구매했습니다.%n";
+	private static final String LOTTERY_TICKET_NUMBER_MESSAGE = "[%s]%n";
+	private static final String WINNING_COUNT_LIST_MESSAGE = "당첨 통계";
+	private static final String LINE_MESSAGE = "--------------------";
+	private static final String LOTTERY_PRIZE_MESSAGE = "%d개 일치 (%d) - %d %n";
+	private static final String REVENUE_MESSAGE = "총 수익률은 %.2f 입니다. (금액 대비 기준은 1 입니다.)";
+
+	public static void buyLotteryTicket(LotteryTicketCount lotteryTicketCount) {
+		System.out.printf(BUY_LOTTERY_TICKET_MESSAGE,
+			lotteryTicketCount.getManualLotteryCount(), lotteryTicketCount.getAutomaticLotteryCount());
+	}
+
+	public static void lotteryTicket(LotteryTickets lotteryTickets) {
+		for (int i = 0; i < lotteryTickets.size(); i++) {
+			System.out.printf(LOTTERY_TICKET_NUMBER_MESSAGE, lotteryTickets.findLotteryTicketNumber(i));
+		}
+	}
+
+	public static void resultWinning(LotteryRanking lotteryRanking) {
+		System.out.println("");
+		System.out.println(WINNING_COUNT_LIST_MESSAGE);
+		System.out.println(LINE_MESSAGE);
+		for (int key : lotteryRanking.keySet()) {
+			System.out.printf(LOTTERY_PRIZE_MESSAGE, key, LotteryPrize.winningAmount(key, 1),
+				lotteryRanking.countWinningNumber(key));
+		}
+	}
+
+	public static void resultRevenue(double revenue) {
+		System.out.printf(REVENUE_MESSAGE, revenue);
+	}
+}
