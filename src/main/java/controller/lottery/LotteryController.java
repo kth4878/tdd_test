ab9962f5acec2a5c5cf5a6368ce1097e17d25c7f
user <kth4878@naver.com>
2021-10-20 20:46:04 +0900

    refactor : 컨트롤러 줄맞춤 수정

--- src/main/java/controller/lottery/LotteryController.java
+++ src/main/java/controller/lottery/LotteryController.java
@@ -11,22 +11,27 @@ import view.lottery.InputView;
 import view.lottery.OutputView;
 
 public class LotteryController {
-	public void run(){
+	public void run() {
 		InputView.lotteryPrice();
 		LotteryStore lotteryStore = new LotteryStore(InputView.inputLotteryPurchasePrice());
-		LotteryTicketCount lotteryTicketCount = new LotteryTicketCount(lotteryStore, InputView.inputManualLotteryCount());
+
+		LotteryTicketCount lotteryTicketCount =
+			new LotteryTicketCount(lotteryStore, InputView.inputManualLotteryCount());
+
 		LotteryTickets lotteryTickets =
-			LotteryService.issueLotteryTicket(InputView.inputManualLotteryNumbers(lotteryTicketCount), lotteryTicketCount);
+			LotteryService.issueLotteryTicket(
+				InputView.inputManualLotteryNumbers(lotteryTicketCount), lotteryTicketCount);
 
 		OutputView.buyLotteryTicket(lotteryTicketCount);
-
 		OutputView.lotteryTicket(lotteryTickets);
 
-		LastWeekLotteryTicket lastWeekWinningNumber = LotteryService.lastWeekWinningNumber(InputView.inputLastWeekLotteryNumbers());
+		LastWeekLotteryTicket lastWeekWinningNumber =
+			LotteryService.lastWeekWinningNumber(InputView.inputLastWeekLotteryNumbers());
+
 		BonusBall bonusBall = new BonusBall(InputView.inputBonusBall());
 
-		LotteryRank lotteryRank = new LotteryRank(
-				LotteryService.lotteryRankingCount(lotteryTickets, lastWeekWinningNumber, bonusBall));
+		LotteryRank lotteryRank =
+			new LotteryRank(LotteryService.lotteryRankingCount(lotteryTickets, lastWeekWinningNumber, bonusBall));
 
 		OutputView.resultWinning(lotteryRank);
 		OutputView.resultRevenue(LotteryService.revenue(lotteryStore, lotteryRank));
