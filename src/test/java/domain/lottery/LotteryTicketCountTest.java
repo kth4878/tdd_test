2e4b908899d9e00fbdc54b99ec96792985a7be86
user <kth4878@naver.com>
2021-10-20 20:40:51 +0900

    test : 수동복권 테스트 추가

--- src/test/java/domain/lottery/LotteryTicketCountTest.java
+++ src/test/java/domain/lottery/LotteryTicketCountTest.java
@@ -8,9 +8,16 @@ import org.junit.jupiter.api.Test;
 class LotteryTicketCountTest {
 
 	@Test
+	@DisplayName("수동복권 수 생성")
+	public void createManualLottery(){
+		LotteryTicketCount lotteryTicket = new LotteryTicketCount(new LotteryStore(10000),2);
+		assertThat(lotteryTicket.getManualLotteryCount()).isEqualTo(2);
+	}
+
+	@Test
 	@DisplayName("자동복권 수 생성")
 	public void createAutomaticLottery(){
-		LotteryTicketCount lotteryTicket = new LotteryTicketCount(new LotteryStore(10),2);
+		LotteryTicketCount lotteryTicket = new LotteryTicketCount(new LotteryStore(10000),2);
 		assertThat(lotteryTicket.getAutomaticLotteryCount()).isEqualTo(8);
 	}
 
