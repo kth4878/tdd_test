f5fe9764b7dc5c157cf861ecb9c5c3ff05c9faef
user <kth4878@naver.com>
2021-10-22 03:22:10 +0900

    remove : 불필요 클래스 제거

--- src/main/java/RaceCarApplication.java
+++ src/main/java/RaceCarApplication.java
@@ -2,7 +2,6 @@ import controller.lottery.LotteryController;
 
 public class LotteryApplication {
 	public static void main(String[] args) {
-		LotteryController lotteryController = new LotteryController();
-		lotteryController.run();
+		
 	}
 }
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,16 @@
+package view.lottery;
+
+import java.util.Scanner;
+
+public class InputView {
+
+	private final static Scanner scanner = new Scanner(System.in);
+
+	public static String[] inputRaceCar(){
+		System.out.println("경주용 자동차 이름을 입력하세요(이름은 콤마(,) 를 기준으로 구분합니다.)");
+
+		return new String[0];
+	}
+
+
+}
