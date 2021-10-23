9314856e0f88ea32e4e1ca3f156d6978867448a7
user <kth4878@naver.com>
2021-10-24 01:04:49 +0900

    refactor : view 기능수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,16 +1,49 @@
-package view.lottery;
+package view;
 
 import java.util.Scanner;
 
 public class InputView {
-
 	private final static Scanner scanner = new Scanner(System.in);
+	private static final String RACE_CAR_INPUT_MESSAGE = "경주용 자동차 이름을 입력하세요(이름은 콤마(,) 를 기준으로 구분합니다.)";
+	private static final String CAR_RACE_COUNT_INPUT_MESSAGE = "경주 횟수를 입력하세요(아라비아 숫자)";
+	private static final String NUMBER_TYPE_ERROR_MESSAGE = "error : 아라비아 숫자만 입력 가능합니다.";
+	private static final String NULL_ERROR_MESSAGE = "error : NULL 값은 입력할수 없습니다.";
+	private static final String EMPTY_ERROR_MESSAGE = "error : 공백은 입력할수 없습니다.";
 
-	public static String[] inputRaceCar(){
-		System.out.println("경주용 자동차 이름을 입력하세요(이름은 콤마(,) 를 기준으로 구분합니다.)");
+	public static String inputRaceCar() {
+		System.out.println(RACE_CAR_INPUT_MESSAGE);
 
-		return new String[0];
+		String carName = scanner.nextLine();
+		checkCarNameNull(carName);
+		checkCarNameEmpty(carName);
+
+		return carName;
 	}
 
+	public static int inputCarRaceCount() {
+		System.out.println(CAR_RACE_COUNT_INPUT_MESSAGE);
+
+		return checkNumberType();
+	}
+
+	private static void checkCarNameNull(String carName) {
+		if (carName == null) {
+			throw new IllegalArgumentException(NULL_ERROR_MESSAGE);
+		}
+	}
+
+	private static void checkCarNameEmpty(String carName) {
+		if (carName.isEmpty()) {
+			throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
+		}
+	}
+
+	private static int checkNumberType() {
+		if (!scanner.hasNextInt()) {
+			throw new IllegalArgumentException(NUMBER_TYPE_ERROR_MESSAGE);
+		}
+
+		return scanner.nextInt();
+	}
 
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,41 +1,22 @@
-package view.lottery;
+package view;
 
-import domain.lottery.LotteryPrize;
-import domain.lottery.LotteryRank;
-import domain.lottery.LotteryTicketCount;
-import domain.lottery.LotteryTickets;
+import domain.CarRaceGroup;
 
 public class OutputView {
+	private static final String RESULT_MESSAGE = "실행결과";
+	private static final String CAR_RACE_RESULT_MESSAGE = "%s : %s";
+	private static final String WINNER_MESSAGE = "%s가 최종 우승했습니다";
 
-	private static final String BUY_LOTTERY_TICKET_MESSAGE = "수동으로 %d장, 자동으로 %d장 을 구매했습니다.%n";
-	private static final String LOTTERY_TICKET_NUMBER_MESSAGE = "[%s]%n";
-	private static final String WINNING_COUNT_LIST_MESSAGE = "당첨 통계";
-	private static final String LINE_MESSAGE = "--------------------";
-	private static final String LOTTERY_PRIZE_MESSAGE = "%d등 (%d) - %d %n";
-	private static final String REVENUE_MESSAGE = "총 수익률은 %.2f 입니다. (금액 대비 기준은 1 입니다.)";
+	public static void resultCarRace(CarRaceGroup carRaceGroup){
+		System.out.println(RESULT_MESSAGE);
 
-	public static void buyLotteryTicket(LotteryTicketCount lotteryTicketCount) {
-		System.out.printf(BUY_LOTTERY_TICKET_MESSAGE,
-			lotteryTicketCount.getManualLotteryCount(), lotteryTicketCount.getAutomaticLotteryCount());
-	}
-
-	public static void lotteryTicket(LotteryTickets lotteryTickets) {
-		for (int i = 0; i < lotteryTickets.size(); i++) {
-			System.out.printf(LOTTERY_TICKET_NUMBER_MESSAGE, lotteryTickets.findLotteryTicketNumber(i));
+		for(int i=0; i<carRaceGroup.size(); i++){
+			System.out.printf(CAR_RACE_RESULT_MESSAGE,carRaceGroup.findCar(i),carRaceGroup.findPosition(i));
+			System.out.println("");
 		}
 	}
 
-	public static void resultWinning(LotteryRank lotteryRank) {
-		System.out.println("");
-		System.out.println(WINNING_COUNT_LIST_MESSAGE);
-		System.out.println(LINE_MESSAGE);
-		for (int key : lotteryRank.keySet()) {
-			System.out.printf(LOTTERY_PRIZE_MESSAGE, key, LotteryPrize.winningAmount(key, 1),
-				lotteryRank.countWinningNumber(key));
-		}
-	}
-
-	public static void resultRevenue(double revenue) {
-		System.out.printf(REVENUE_MESSAGE, revenue);
+	public static void winner(CarRaceGroup carRaceGroup){
+		System.out.printf(WINNER_MESSAGE,carRaceGroup.findWinner());
 	}
 }
