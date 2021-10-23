288eac390cb5ffbb9da795f5c08d210af5b0f279
user <kth4878@naver.com>
2021-10-24 01:04:29 +0900

    rafactor : 이름변경으로 인한 main 수정

--- src/main/java/RaceCarApplication.java
+++ src/main/java/RaceCarApplication.java
@@ -1,7 +1,8 @@
-import controller.lottery.LotteryController;
+import controller.CarRaceController;
 
-public class LotteryApplication {
+public class RaceCarApplication {
 	public static void main(String[] args) {
-		
+		CarRaceController raceCarController = new CarRaceController();
+		raceCarController.run();
 	}
 }
