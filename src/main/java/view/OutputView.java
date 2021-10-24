297d5a8531903a79e2555a227bd67841a1cccd1f
user <kth4878@naver.com>
2021-10-24 22:56:58 +0900

    refactor : for문 stream 으로 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,19 +1,20 @@
 package view;
 
+import java.util.Arrays;
+
 import domain.CarRaceGroup;
 
 public class OutputView {
 	private static final String RESULT_MESSAGE = "실행결과";
-	private static final String CAR_RACE_RESULT_MESSAGE = "%s : %s";
-	private static final String WINNER_MESSAGE = "%s가 최종 우승했습니다";
+	private static final String CAR_RACE_RESULT_MESSAGE = "%s : %s %n";
+	private static final String WINNER_MESSAGE = "%s가 최종 우승했습니다.";
 
 	public static void resultCarRace(CarRaceGroup carRaceGroup) {
 		System.out.println(RESULT_MESSAGE);
 
-		for (int i = 0; i < carRaceGroup.size(); i++) {
-			System.out.printf(CAR_RACE_RESULT_MESSAGE, carRaceGroup.findCar(i), carRaceGroup.findPosition(i));
-			System.out.println("");
-		}
+		carRaceGroup.getCarRaceGroup()
+			.stream()
+			.forEach(i -> System.out.printf(CAR_RACE_RESULT_MESSAGE, i.getCar(), i.getPosition()));
 	}
 
 	public static void winner(CarRaceGroup carRaceGroup) {
